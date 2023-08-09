2f5c53e962cb11245fbea374de4f443bddcee1c0
Gebrecherkos Abrha <73259009+GebrecherkosAb@users.noreply.github.com>
2023-08-10 00:17:07 +0200

    Update script and schema for improved data generation

--- VehicleDataGenerator.py
+++ VehicleDataGenerator.py
@@ -23,11 +23,32 @@ def generate_csv_files(schema, output_directory, num_files, rows_per_file):
                     field_type = field['type']
 
                     if field_type == 'string':
-                        record[field_name] = fake.word()
+                        if field_name == 'Car_Name':
+                            record[field_name] = fake.word()
+                        elif field_name == 'Fuel_Type':
+                            record[field_name] = fake.random_element(elements=('Petrol', 'Diesel', 'CNG'))
+                        elif field_name == 'Seller_Type':
+                            record[field_name] = fake.random_element(elements=('Dealer', 'Individual'))
+                        elif field_name == 'Transmission':
+                            record[field_name] = fake.random_element(elements=('Manual', 'Automatic'))
+                        elif field_name == 'Car_Type':
+                            record[field_name] = fake.random_element(elements=('Sedan', 'SUV', 'Hatchback'))
+                        elif field_name == 'Location':
+                            record[field_name] = fake.city()
+                        else:
+                            record[field_name] = fake.word()
                     elif field_type == 'int':
-                        record[field_name] = fake.random_int(min=2010, max=2022)
+                        if field_name == 'Year':
+                            record[field_name] = fake.random_int(min=2000, max=2022)
+                        elif field_name == 'Owner':
+                            record[field_name] = fake.random_int(min=0, max=3)
+                        else:
+                            record[field_name] = fake.random_int(min=0, max=1000)
                     elif field_type == 'float':
-                        record[field_name] = round(random.uniform(10000, 30000), 2)
+                        if field_name == 'Selling_Price' or field_name == 'Present_Price':
+                            record[field_name] = round(random.uniform(1, 20), 2)
+                        else:
+                            record[field_name] = round(random.uniform(1000, 50000), 2)
 
                 writer.writerow(record)
 
@@ -35,7 +56,7 @@ if __name__ == '__main__':
     num_files = 10
     rows_per_file = 100
     schema_file = 'vehicle_schema.yaml'
-    output_directory = 'vehicle_data_output'  # Use a descriptive name
+    output_directory = 'vehicle_data_output'
 
     with open(schema_file, 'r') as f:
         schema = yaml.safe_load(f)
